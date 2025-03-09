5dc367fcd9b2e416e2481a0123cb0b88d943c78d
ramazanturker <ramazantturker@hotmail.com>
2025-03-10 00:20:48 +0300

    django courseapp project

--- courses/admin.py
+++ courses/admin.py
@@ -5,15 +5,23 @@ from .models import Course, Category
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ("title", "isActive", "slug",)
+    list_display = ("title", "isActive", "slug", "category_list",)
     list_display_links = ("title", "slug",)
     prepopulated_fields = { "slug": ("title",), }
     list_filter = ("title", "isActive",)
     list_editable = ("isActive",)
     search_fields = ("title", "description",)
+    
+    def category_list(self, object):
+        html = ""
+        for category in object.categories.all():
+            html += category.name + " "
+        return html
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("name", "slug")
+    list_display = ("name", "slug", "course_count")
     prepopulated_fields = { "slug": ("name",), }
-        
+    
+    def course_count(self, object):
+        return object.course_set.count()
--- courses/views.py
+++ courses/views.py
@@ -1,59 +1,12 @@
 from datetime import date, datetime
-from django.http import Http404, HttpResponseNotFound
-from django.shortcuts import get_object_or_404, redirect, render
-from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
 from .models import Course, Category
+from django.core.paginator import Paginator
 
-data = {
-    "programming": "course lists in the programming category",
-    "web-development": "course lists in the web development category",
-    "mobile": "course lists in the mobile category",
-}
-
-db = {
-    "courses": [
-        {
-            "title": "javascript course",
-            "description": "javascript course description",
-            "imageUrl": "1.jpg",
-            "slug": "javascript-course",
-            "date": datetime.now(),
-            "isActive": True,
-            "isUpdated": False
-        },
-        {
-            "title": "python course",
-            "description": "python course description",
-            "imageUrl": "2.jpg",
-            "slug": "python-course",
-            "date": date(2022,9,10),
-            "isActive": True,
-            "isUpdated": False
-        },
-        {
-            "title": "web development course",
-            "description": "web development course description",
-            "imageUrl": "3.jpg",
-            "slug": "web-development-course",
-            "date": date(2022,8,10),
-            "isActive": True,
-            "isUpdated": True
-        },
-    ],
-    "categories": [
-        { "id": 1, "name": "programming", "slug": "programming" },
-        { "id": 2, "name": "web development", "slug": "web-development" },
-        { "id": 3, "name": "mobile applications", "slug": "mobile-applications" },
-    ]
-}
 
 def index(request):
     courses = Course.objects.filter(isActive = 1)
     categories = Category.objects.all()
-
-    # for course in db["courses"]:
-    #     if course["isActive"] == True:
-    #         courses.append(course)
     
     return render(request, 'courses/index.html', {
         'categories': categories,
@@ -69,11 +22,15 @@ def details(request, slug):
     return render(request, 'courses/details.html', context)
 
 def getCoursesByCategory(request, slug):
-    courses = Course.objects.filter(category__slug=slug, isActive=True)
+    courses = Course.objects.filter(categories__slug=slug, isActive=True).order_by("date")
     categories = Category.objects.all()
+    
+    paginator = Paginator(courses, 3)
+    page = request.GET.get('page', 1)
+    page_object = paginator.page(page)
     
     return render(request, 'courses/index.html', {
         'categories': categories,
-        'courses': courses,
+        'page_object': page_object,
         'selectedCategory': slug
     })
