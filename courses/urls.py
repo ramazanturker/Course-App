50f11492aa9cbe09d9071891e56abf5c0b388d45
ramazanturker <ramazantturker@hotmail.com>
2025-03-10 18:02:01 +0300

    django courseapp project

--- courses/urls.py
+++ courses/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="index"),
+    path('search', views.search, name="search"),
+    path('create-course', views.create_course, name="create_course"),
     path('<slug:slug>', views.details, name="course_details"),
     path('category/<slug:slug>', views.getCoursesByCategory, name='courses_by_category'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,16 +1,64 @@
 from datetime import date, datetime
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Course, Category
 from django.core.paginator import Paginator
 
 
 def index(request):
-    courses = Course.objects.filter(isActive = 1)
+    courses = Course.objects.filter(isActive = 1, isHome=1)
     categories = Category.objects.all()
     
     return render(request, 'courses/index.html', {
         'categories': categories,
         'courses': courses
+    })
+    
+def create_course(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        description = request.POST["description"]
+        imageUrl = request.POST["imageUrl"]
+        slug = request.POST["slug"]
+        isActive = request.POST.get("isActive", False)
+        isHome = request.POST.get("isHome", False)
+        
+        if isActive == "on":
+            isActive = True
+            
+        if isHome == "on":
+            isHome = True
+        
+        error = False
+        message = ""
+        
+        if title == "":
+            error = True
+            message += "title is a required field"
+        
+        if len(title) < 5:
+            error = True
+            message += "you must enter at least five characters"
+        
+        if error:
+            return render(request, 'courses/create-course.html', { "error": True, "message": message })
+             
+        course = Course(title = title, description = description, imageUrl = imageUrl, slug = slug, isActive = isActive, isHome = isHome)
+        course.save()
+        return redirect("/courses")
+        
+    return render(request, 'courses/create-course.html')
+    
+def search(request):
+    if "q" in request.GET and request.GET["q"] != "":
+        q = request.GET["q"]
+        courses = Course.objects.filter(isActive=True, title__contains=q).order_by("date")
+        categories = Category.objects.all()
+    else:
+        return redirect("/courses")
+    
+    return render(request, 'courses/search.html', {
+        'categories': categories,
+        'courses': courses,
     })
 
 def details(request, slug):
@@ -29,7 +77,7 @@ def getCoursesByCategory(request, slug):
     page = request.GET.get('page', 1)
     page_object = paginator.page(page)
     
-    return render(request, 'courses/index.html', {
+    return render(request, 'courses/list.html', {
         'categories': categories,
         'page_object': page_object,
         'selectedCategory': slug
