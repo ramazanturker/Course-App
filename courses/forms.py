e87a87ea8b0709824aa2fcbd0516919f4dd7d99e
ramazanturker <ramazantturker@hotmail.com>
2025-03-10 23:19:58 +0300

    django courseapp project

--- courses/forms.py
+++ courses/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 
 class CourseCreateForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField(widget=forms.Textarea)
-    imageUrl = forms.CharField()
-    slug = forms.SlugField()
+    title = forms.CharField(
+        label="course title", 
+        required=True,
+        error_messages={
+            "required":"you must enter the course title"}, 
+        widget=forms.TextInput(attrs={ "class":"form-control" }))
+    
+    description = forms.CharField(widget=forms.Textarea(attrs={ "class":"form-control" }))
+    imageUrl = forms.CharField(widget=forms.TextInput(attrs={ "class":"form-control" }))
+    slug = forms.SlugField(widget=forms.TextInput(attrs={ "class":"form-control" }))
