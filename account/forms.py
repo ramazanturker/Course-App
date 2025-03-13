9b52f249d1f6d3c5b30173dd64887f3c36974f8b
ramazanturker <ramazantturker@hotmail.com>
2025-03-13 22:17:16 +0300

    django courseapp project

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
 from django.forms import widgets
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 class LoginUserForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
@@ -20,3 +21,41 @@ class LoginUserForm(AuthenticationForm):
     # def confirm_login_allowed(self, user):
     #     if user.username.startswith("r"):
     #         raise forms.ValidationError("you cannot log in with this username")
+    
+class NewUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "username", "email",)
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["first_name"].widget = widgets.TextInput(attrs={ "class": "form-control"})
+        self.fields["last_name"].widget = widgets.TextInput(attrs={ "class": "form-control"})
+        self.fields["password1"].widget = widgets.PasswordInput(attrs={ "class": "form-control"})
+        self.fields["password2"].widget = widgets.PasswordInput(attrs={ "class": "form-control"})
+        self.fields["username"].widget = widgets.TextInput(attrs={ "class": "form-control"})
+        self.fields["email"].widget = widgets.EmailInput(attrs={ "class": "form-control"})
+        self.fields["email"].required = True
+        
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        
+        if User.objects.filter(email = email).exists():
+            self.add_error("email", "this email is already in use")
+            
+        return email
+
+class UserPasswordChangeForm(PasswordChangeForm):        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["new_password1"].widget = widgets.PasswordInput(attrs={ "class": "form-control"})
+        self.fields["new_password2"].widget = widgets.PasswordInput(attrs={ "class": "form-control"})
+        self.fields["old_password"].widget = widgets.PasswordInput(attrs={ "class": "form-control"})
+        
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        
+        if User.objects.filter(email = email).exists():
+            self.add_error("email", "this email is already in use")
+            
+        return email
--- account/views.py
+++ account/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.models import User
 from django.contrib import messages
-from account.forms import LoginUserForm
-from django.contrib.auth.forms import UserCreationForm
+from account.forms import LoginUserForm, NewUserForm, UserPasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 
 # Create your views here.
 def user_login(request):
@@ -36,7 +36,7 @@ def user_login(request):
 
 def user_register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = NewUserForm(request.POST)
         
         if form.is_valid():
             form.save()
@@ -49,8 +49,21 @@ def user_register(request):
         else:
             return render(request, "account/register.html", { "form": form })
     else:
-        form = UserCreationForm()
+        form = NewUserForm()
         return render(request, "account/register.html", { "form": form })
+    
+def change_password(request):
+    if request.method == "POST":
+        form = UserPasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, "password updated")
+            return redirect("change_password")
+        else:
+            return render(request, "account/change-password.html", { "form": form })
+    form = UserPasswordChangeForm(request.user)
+    return render(request, "account/change-password.html", { "form": form })
 
 def user_logout(request):
     messages.add_message(request, messages.SUCCESS, "logout successful")
