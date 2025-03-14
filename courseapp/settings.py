6e8ec10398f5dbd2a3f1368b1d230e8a71b7b9e9
ramazanturker <ramazantturker@hotmail.com>
2025-03-14 18:55:02 +0300

    django courseapp project

--- courseapp/settings.py
+++ courseapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from django.contrib.messages import constants as messages
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4ln_#vkd(+jthb=36%1-p(f-bk)sshy+!-hqd!l$7dgc4y4wsb'
+# SECRET_KEY = 'django-insecure-4ln_#vkd(+jthb=36%1-p(f-bk)sshy+!-hqd!l$7dgc4y4wsb'
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("IS_DEVELOPMENT", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv("APP_HOST")
+]
 
 
 # Application definition
@@ -122,6 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
--- courseapp/urls.py
+++ courseapp/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('courses/', include('courses.urls')),
     path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
